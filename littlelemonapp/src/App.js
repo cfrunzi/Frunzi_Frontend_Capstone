34fec69b0eab06857341ffb87e7ac5e74df9066b
cfrunzi <cfrunzi@alumni.uncc.edu>
2023-03-26 07:05:26 -0400

    added footer

--- littlelemonapp/src/App.js
+++ littlelemonapp/src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Nav from "./page_layouts/Nav"
+import Footer from "./page_layouts/Footer"
 import Homepage from "./pages/Homepage"
 import './App.css';
 
@@ -13,6 +14,7 @@ function App() {
       <Route path="/booking"/>
       <Route path="/booking-confirmation"/>
     </Routes>
+    <Footer/>
     </BrowserRouter>
   );
 }
