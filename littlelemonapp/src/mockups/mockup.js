1d0a20eff6f7bf3d5326bcd86f61e7e6fb913095
cfrunzi <cfrunzi@alumni.uncc.edu>
2023-03-26 07:24:23 -0400

    added form logic for selecting date

---
+++ littlelemonapp/src/mockups/mockup.js
@@ -0,0 +1,29 @@
+const randomSeed = function(seed){
+    var max = 2 ** 35 - 31;
+    var a = 185852;
+    var s = seed % max;
+    return function(){
+        return(s = (s * a) % max) / max;
+    };
+};
+
+// fetch the current date from the API
+export const fetchAPI = function(date){
+    let result = [];
+    let rand = randomSeed(date.getDate());
+
+    for(let i = 17; i <= 23; i++){
+        if(rand() < 0.5){
+            result.push(i + ":00");
+        }
+        if(rand() > 0.5){
+            result.push(i + ":30");
+        }
+    }
+
+    return result;
+};
+
+export const submitAPI = function(formData){
+    return true;
+};
