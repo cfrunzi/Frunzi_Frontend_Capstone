cd8c4d2f9a0dbd76aefdc94c72cf14d02f6ef216
cfrunzi <cfrunzi@alumni.uncc.edu>
2023-03-26 07:14:09 -0400

    updated directory for assets and created new component files

---
+++ littlelemonapp/src/pages/Booking.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Restaurant from "../assets/restaurant_inside.jpg"
+
+
+const Booking = () =>{
+    const navigate = useNavigate();
+    return(<></>);
+}
+
