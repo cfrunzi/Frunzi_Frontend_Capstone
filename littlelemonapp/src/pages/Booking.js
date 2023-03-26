ea780c555cd34f9f0acb6499eb137a5bd1ac05f6
cfrunzi <cfrunzi@alumni.uncc.edu>
2023-03-26 09:00:00 -0400

    added HTML for booking page and CSS rules

--- littlelemonapp/src/pages/Booking.js
+++ littlelemonapp/src/pages/Booking.js
@@ -1,10 +1,22 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import Restaurant from "../assets/restaurant_inside.jpg"
+import BookingForm from "../components/BookingForm";
+import './Booking.css'
 
 
 const Booking = () =>{
     const navigate = useNavigate();
-    return(<></>);
+    return(
+    <main className="booking-page">
+        <h1>Booking a Table</h1>
+        <section>
+            <img src={Restaurant} alt="Our indoor view" style={{borderRadius: "8px", width: "65%"}}/>
+        </section>
+        <section>
+            <BookingForm navigate={navigate}/>
+        </section>
+    </main>);
 }
 
+export default Booking;
