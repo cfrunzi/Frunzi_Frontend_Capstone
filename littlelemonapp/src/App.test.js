3cc3d6ade367b2cd44a3b69d9f3d158e80815a62
cfrunzi <cfrunzi@alumni.uncc.edu>
2023-03-26 10:59:19 -0400

    added unit tests for project

--- littlelemonapp/src/App.test.js
+++ littlelemonapp/src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from '@testing-library/react';
+import BookingForm from './components/BookingForm';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test to see whether booking form renders correctly
+test("Renders the reservation form", () => {
+  render(<BookingForm/>);
+  const submit = screen.getByText("Make Reservation");
+  expect(submit).toBeInTheDocument();
+})
+
+test("Updates date correctly", () =>{
+  render(<BookingForm/>);
+  const dateSelect = screen.getByLabelText(/Pick a date/);
+  fireEvent.change(dateSelect, {target: {value: "2023-03-26"}});
+})
